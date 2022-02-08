875e3bd423ec8854ef41b95ddf4b395feec137dd
gayatri2 <gayatri.godbole02@gmail.com>
2022-02-08 12:16:16 +0530

    Added graph algorithms.

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -1,8 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define V 4	//No of vertices
-
+#define V 6		//No of vertices
 int selectMinVertex(vector<int>& value,vector<bool>& setMST)
 {
 	int minimum = INT_MAX;
@@ -17,10 +16,9 @@ int selectMinVertex(vector<int>& value,vector<bool>& setMST)
 	}
 	return vertex;
 }
-
-void findMST(int graph[V][V])
+void dijkstra(int graph[V][V])
 {
-	int parent[V];		//helps in printing the MST structure
+	int parent[V];		//helps in printing the structure
 	vector<int> weight(V,INT_MAX);	////track of edge weight
 	vector<bool> setMST(V,false);	////to keep track of visited nodes
 
@@ -28,7 +26,7 @@ void findMST(int graph[V][V])
 	parent[0] = -1;	//Start node has no parent
 	weight[0] = 0;	//start node has value=0 to get picked 1st
 
-   //MST is V vertices, V-1 edges. So, algorithm has to be processed V-1 times. ie outer loop is the no of times all steps processed
+   //V vertices, V-1 edges. So, algorithm has to be processed V-1 times. ie outer loop is the no of times all steps processed
 	
 	for(int i=0;i<V-1;++i)
 	{
@@ -39,14 +37,15 @@ void findMST(int graph[V][V])
 		//Go to adjacent vertices (not yet included in MST)
 		for(int j=0;j<V;++j)
 		{
-			/* 3 constraints to relax:-
+			/* 3 conditions to relax:-
 			      1.Edge is present from U to j.
-			      2.Vertex j is not included in MST
+			      2.Vertex j is not included in shortest path graph
 			      3.Edge weight is smaller than current edge weight
 			*/
-			if(graph[U][j]!=0 && setMST[j]==false && weight[U]!=INT_MAX && graph[U][j]<weight[j])
+            
+			if(graph[U][j]!=0 && setMST[j]==false && weight[U]!=INT_MAX && (weight[U]+graph[U][j]<weight[j]))
 			{
-				weight[j] = graph[U][j];
+				weight[j] = weight[U]+graph[U][j];
 				parent[j] = U;
 			}
 		}
@@ -55,28 +54,30 @@ void findMST(int graph[V][V])
 	for(int i=1;i<V;++i)
 		cout<<"U->V: "<<parent[i]<<"->"<<i<<"  wt = "<<graph[parent[i]][i]<<"\n";
 
-    int cost=0;
-    for(int i=1;i<4;i++)
-    {
-        cost+=graph[parent[i]][i];
-
-    }
-    cout << endl;
-    cout << "Cost:" << cost << endl;
+   cout<< "Minimum cost to reach the vertices from source vertex:"<<endl;
+   for(int i=0;i<V;++i)
+   {
+       cout << weight[i] << " ";
+   }
 }
 
 int main()
 {
-	int graph[V][V] = { 
-                        {0,3,1,0},
-                        {3,0,2,4},
-                        {1,2,0,5},
-                        {4,5,0,0}
-                        
-                    };
+	int graph[V][V] = { {0, 1, 4, 0, 0, 0},
+						{1, 0, 4, 2, 7, 0},
+						{4, 4, 0, 3, 5, 0},
+						{0, 2, 3, 0, 4, 6},
+						{0, 7, 5, 4, 0, 7},
+						{0, 0, 0, 6, 7, 0} };
 
-	findMST(graph);	
+    for(int i=0;i<V;i++)
+    {
+        for(int j=0;j<V; j++)
+        {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+	dijkstra(graph);	
 	return 0;
 }
-
-//TIME COMPLEXITY: O(V^2)
